6c97d165f38bda2c198e1649dabe5bb27f152106
Koltsun Nikita <koltsun-nv@yandex.ru>
2023-01-25 02:31:53 +0300

    auth

---
+++ js/main.js
@@ -0,0 +1,84 @@
+const cartButton = document.querySelector('#cart-button');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+
+cartButton.addEventListener('click', toggleModal);
+close.addEventListener('click', toggleModal);
+
+function toggleModal(){
+    modal.classList.toggle('is-open');
+}
+
+//day 1 
+
+const buttonAuth =  document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const loginForm = document.querySelector('#logInForm');
+const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+
+let login = localStorage.getItem('Delivery');
+
+function toogleModalAuth(){
+    modalAuth.classList.toggle('is-open');
+};
+
+
+function authorized(){
+
+    function logOut(){
+        login = null;
+        localStorage.removeItem('Delivery');
+        buttonAuth.style.display = '';
+        userName.style.display = '';
+        buttonOut.style.display = '';
+        buttonOut.removeEventListener('click', logOut);
+
+        checkAuth();
+    }
+
+    console.log('Авторизован');
+
+    userName.textContent = login;
+
+    buttonAuth.style.display = 'none';
+    userName.style.display = 'inline';
+    buttonOut.style.display = 'block';
+    buttonOut.addEventListener('click', logOut);
+}
+
+function notAuthorized() {
+    console.log('Не авторизован');
+
+    function logIn(event){
+        event.preventDefault();
+        login = loginInput.value;
+
+        localStorage.setItem('Delivery', login);
+
+        toogleModalAuth();
+        buttonAuth.removeEventListener('click', toogleModalAuth);
+        closeAuth.removeEventListener('click', toogleModalAuth);
+        loginForm.removeEventListener('submit', logIn);
+        loginForm.reset();
+
+        checkAuth();
+    };
+
+    buttonAuth.addEventListener('click', toogleModalAuth);
+    closeAuth.addEventListener('click', toogleModalAuth);
+    loginForm.addEventListener('submit', logIn);
+};
+
+function checkAuth(){
+    if (login){
+        authorized();
+    }else{
+        notAuthorized();
+    }
+}
+
+checkAuth();
