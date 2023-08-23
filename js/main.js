64485fccfcac955b688ba9af222cdd6196e11a59
Koltsun Nikita <koltsun-nv@yandex.ru>
2023-08-23 22:37:47 +0300

    v4.0.0

--- js/main.js
+++ js/main.js
@@ -34,6 +34,10 @@ const category = document.querySelector(".category");
 // Поиск
 const inputSearch = document.querySelector(".input-search");
 
+// Корзина
+const modalBody = document.querySelector('.modal-body');
+const modalPrice = document.querySelector(".modal-pricetag");
+const buttonClearCart = document.querySelector(".clear-cart")
 
 const  getData = async (url) =>{
 
@@ -166,7 +170,7 @@ const createCardRestaurant = (restaurant) =>{
 
 const createCardFood = (goods) =>{
 
-    const { name, description, price, image } = goods;
+    const { name, description, price, image, id } = goods;
 
     const card = document.createElement('div');
     card.className = 'card';
@@ -187,11 +191,11 @@ const createCardFood = (goods) =>{
             </div>
 
             <div class="card-buttons">
-                <button class="button button-primary button-add-cart">
+                <button class="button button-primary button-add-cart" id=${id}>
                     <span class="button-card-text">В корзину</span>
                     <span class="button-cart-svg"></span>
                 </button>
-                <strong class="card-price-bold">${price} ₽</strong>
+                <strong class="card-price card-price-bold">${price} ₽</strong>
             </div>
         </div>
 
@@ -241,6 +245,87 @@ const openGoods = (event) => {
 
 };
 
+const addToCart = (event) => {
+    const target = event.target;
+    const buttonAddToCart = target.closest('.button-add-cart');
+
+    if (buttonAddToCart){
+        const card = target.closest('.card');
+        const title = card.querySelector(".card-title-reg").textContent;
+        const cost = card.querySelector(".card-price").textContent;
+        const id = buttonAddToCart.id;
+
+        const food = cart.find(function(item){
+            return item.id === id
+        })
+
+        if (food){
+            food.count += 1;
+        } else {
+            cart.push({
+                id: id,
+                title: title,
+                cost: cost,
+                count: 1
+            });
+        }
+
+    }
+    
+}
+
+const renderCart = () =>{
+    modalBody.textContent = '';
+    cart.forEach(function({ id, title, cost, count }){
+        const itemCart = `
+            <div class="food-row">
+                <span class="food-name">${title}</span>
+                <strong class="food-price">${cost}</strong>
+                <div class="food-counter">
+                    <button class="counter-button counter-minus" data-id=${id}>-</button>
+                    <span class="counter">${count}</span>
+                    <button class="counter-button counter-plus" data-id=${id}>+</button>
+                </div>
+            </div>
+        `;
+
+        modalBody.insertAdjacentHTML('afterbegin', itemCart);
+    });
+
+    const totalPrice = cart.reduce(function(result, item) {
+        return result + (parseFloat(item.cost) * item.count);
+    }, 0);
+
+    modalPrice.textContent = totalPrice + ' ₽';
+};
+
+const changeCount = (event) =>{
+    const target = event.target;
+
+    if(target.classList.contains("counter-button")){
+        const food = cart.find(function(item){
+            return item.id === target.dataset.id;
+        });
+
+        if (target.classList.contains('counter-minus')){
+            food.count --;
+            if (food.count === 0){
+                cart.splice(cart.indexOf(food), 1);
+            }
+        }
+
+        if (target.classList.contains('counter-plus')){
+            food.count ++;
+        }
+
+        renderCart();
+    }
+
+    
+
+    
+};
+
 const init = () =>{
     getData('./db/partners.json').then((data) =>{
 
@@ -249,7 +334,19 @@ const init = () =>{
     });
     
     
-    cartButton.addEventListener("click", toogleModal);
+    cartButton.addEventListener("click", function(){
+        renderCart();
+        toogleModal();
+    });
+
+    buttonClearCart.addEventListener('click', function(){
+        cart.length = 0;
+        renderCart();
+    });
+
+    modalBody.addEventListener("click", changeCount);
+
+    cardsMenu.addEventListener("click", addToCart);
     
     close.addEventListener("click", toogleModal);
     
@@ -260,7 +357,7 @@ const init = () =>{
         restaurants.classList.remove('hide');
         menu.classList.add('hide');
     });
-// TODO Доделать поиск
+
     inputSearch.addEventListener('keydown', (event) =>{
         if (event.keyCode === 13){
             const target = event.target;
